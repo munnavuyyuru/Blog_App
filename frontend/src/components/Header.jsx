186869e17b5d775b56f4f94b0a22bf4caeaba124
munnavuyyuru <111678429+munnavuyyuru@users.noreply.github.com>
2025-05-22 23:31:00 +0530

    create header section

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,75 @@
 import React from "react";
+import {
+  Button,
+  Navbar,
+  TextInput,
+  NavbarCollapse,
+  NavbarLink,
+  NavbarToggle,
+} from "flowbite-react";
+import { Link, useLocation } from "react-router-dom";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaMoon } from "react-icons/fa";
 
 const Header = () => {
-  return <div className="text-red-300">Header</div>;
+  const path = useLocation.pathname;
+  return (
+    <Navbar className="border-b-2">
+      <Link
+        to="/"
+        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+      >
+        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+          Bhargav's
+        </span>
+        Blog
+      </Link>
+      <form>
+        <TextInput
+          type="text"
+          placeholder="Search..."
+          rightIcon={AiOutlineSearch}
+          className="hidden lg:inline"
+        />
+      </form>
+
+      <Button className="w-14 h-10 lg:hidden" color="light" pill>
+        <AiOutlineSearch />
+      </Button>
+      <div className="flex gap-2 md:order-2">
+        <Button className="w-12 h-10 hidden sm:inline" color="light" pill>
+          <FaMoon />
+        </Button>
+        <Link to="/sign-in">
+          <Button className="bg-gradient-to-br from-purple-600 to-blue-500 text-white hover:bg-gradient-to-bl focus:ring-blue-300 dark:focus:ring-blue-800 text-sm sm:text-xl">
+            Sign In
+          </Button>
+        </Link>
+      </div>
+      <NavbarToggle />
+      <NavbarCollapse>
+        <NavbarLink active={path === "/"} as={Link} to="/" className="text-xl">
+          Home
+        </NavbarLink>
+        <NavbarLink
+          active={path === "/about"}
+          as={Link}
+          to="/about"
+          className="text-xl"
+        >
+          About
+        </NavbarLink>
+        <NavbarLink
+          active={path === "/projects"}
+          as={Link}
+          to="/projects"
+          className="text-xl"
+        >
+          Projects
+        </NavbarLink>
+      </NavbarCollapse>
+    </Navbar>
+  );
 };
 
 export default Header;
