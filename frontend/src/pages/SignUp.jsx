1523b1fd2b351ce91735f0e81ec2c40eec8e3c94
munnavuyyuru <111678429+munnavuyyuru@users.noreply.github.com>
2025-07-18 22:39:02 +0530

    signin api done

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -47,6 +47,7 @@ const SignUp = () => {
     }
   };
 
+  
   return (
     <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
@@ -112,7 +113,7 @@ const SignUp = () => {
           </form>
           <div className="flex gap-2 text-sm mt-5">
             <span>Have an account?</span>
-            <Link to="/sign-in" className="text-blue-500">
+            <Link to="/sign-in" className="text-md text-blue-500">
               Sign In
             </Link>
           </div>
