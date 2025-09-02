c26f9427ce1ffb795291f67c8564e67d511dbe63
munnavuyyuru <111678429+munnavuyyuru@users.noreply.github.com>
2025-09-02 22:21:32 +0530

    done about & project page

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import CallToAction from "../components/CallToAction";
 
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <div className="min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3">
+      <h1 className="text-3xl font-semibold">Check Out!</h1>
+      <p className="text-md text-gray-500">
+        Build fun and engaging projects while learning..
+      </p>
+      <CallToAction />
+    </div>
+  );
 };
 
 export default Projects;
