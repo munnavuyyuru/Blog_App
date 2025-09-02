cb9f973f4366c47ad3fddec01f811ea7b659b57c
munnavuyyuru <111678429+munnavuyyuru@users.noreply.github.com>
2025-09-02 23:57:38 +0530

    fixed module name in signin page

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -7,7 +7,7 @@ import {
   signInSuccess,
   signInFailure,
 } from "../redux/user/userSlice";
-import OAuth from "../components/Oauth";
+import OAuth from "../components/OAuth";
 
 const SignIn = () => {
   const [formData, setFormData] = useState({});
