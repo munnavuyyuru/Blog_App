e7efa4f9b53ef8d9e5ca6a6f2fed53ba559a4a24
munnavuyyuru <111678429+munnavuyyuru@users.noreply.github.com>
2025-09-03 13:17:01 +0530

    added dynamic port

--- backend/index.js
+++ backend/index.js
@@ -12,8 +12,10 @@ dotenv.config();
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("server is running on port 3000 !");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT} !`);
 });
 
 mongoose
